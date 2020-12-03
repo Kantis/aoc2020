b4ff93fa7c00088013a6df4706431bd856adcd23
Emil Kantis <emil.kantis@protonmail.com>
2020-12-03 15:26:53 +0100

    Improved reading of input

--- solutions/day3.py
+++ solutions/day3.py
@@ -1,15 +1,12 @@
-from dataclasses import dataclass
-
-
 def is_tree(x, y):
-    return lines[y][(x % len(lines[y]))] == '#'
+    return lines[y][x % len(lines[y])] == '#'
 
 
-lines = list(map(lambda x: x.replace('\n', ''), open("../inputs/day3.txt").readlines()))
+lines = open("../inputs/day3.txt").read().splitlines()
 height = len(lines)
 
-print(sum([is_tree(y, y) for y in range(1, height)]))
-print(sum([is_tree(y * 3, y) for y in range(1, height)]))
-print(sum([is_tree(y * 5, y) for y in range(1, height)]))
-print(sum([is_tree(y * 7, y) for y in range(1, height)]))
-print(sum([is_tree(int(y/2), y) for y in range(2, height, 2)]))
+print(sum(is_tree(y, y) for y in range(1, height)))
+print(sum(is_tree(y * 3, y) for y in range(1, height)))
+print(sum(is_tree(y * 5, y) for y in range(1, height)))
+print(sum(is_tree(y * 7, y) for y in range(1, height)))
+print(sum(is_tree(int(y / 2), y) for y in range(2, height, 2)))
